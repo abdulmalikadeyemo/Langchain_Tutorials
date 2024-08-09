6859d8a3bd59d9bb376ff8bb6550811c5617cf82
abdulmalikadeyemo <abdulmalikadeyemo@outlook.com>
2024-08-10 00:46:59 +0100

    added the first tutorial notebook including deployment application files

---
+++ app.py
@@ -0,0 +1,22 @@
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# Create an instance of the llama-3.1-8b-instant model
+model = ChatGroq(model="llama-3.1-8b-instant")
+
+#Create the ouput parser
+parser = StrOutputParser()
+
+#Create the prompt
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system",system_template), ("user", "{text}")]
+)
+
+#Create the chain
+chain = prompt_template | model | parser
---
+++ server.py
@@ -0,0 +1,27 @@
+# Install langserve using "pip install langserve[all]"
+
+from fastapi import FastAPI
+from langserve import add_routes
+import uvicorn
+from app import chain
+
+
+#1. Create the app definition
+app = FastAPI(
+    title="First Langchain Deployment",
+    version="1.0",
+    description="A deployment for out text translation application"
+)
+
+#2. Add the chain to the route
+add_routes(
+    app,
+    chain,
+    path="/translate"
+)
+
+
+if __name__=="__main__":
+
+    uvicorn.run(app, host="localhost", port=8000)
+
